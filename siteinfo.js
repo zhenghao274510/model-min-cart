c0546b4e74933f9e52c982a081b71cc23bff0a84
https://github.com/zhenghao274510 <490462382@qq.com>
2019-11-23 18:23:23 +0800

    model

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "柚子租车（案例）",
+    uniacid: "14",
+    acid: "14",
+    multiid: "0",
+    version: "1.3.2",
+  siteroot: "https://shop.weidayi.com/app/index.php",
+    design_method: "3"
+};
